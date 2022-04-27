ab1f6a96c020a5c3ec8da8392693b2aef4bba196
Juxuny Wu <juxuny@163.com>
2022-04-27 14:52:55 +0800

    init

---
+++ clash-add-ip.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "- \"IP-CIDR,$1,\U0001F343 Proxies\"" >> ~/.config/clash/tpl.yaml
---
+++ clash-add-key.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "- \"DOMAIN-KEYWORD,$1,\U0001F343 Proxies\"" >> ~/.config/clash/tpl.yaml
